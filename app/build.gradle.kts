632a41e699c8f99b88734285ae754b8bb9b3ed2b
wildan <wildanal577@gmail.com>
2023-03-08 13:18:03 +0700

    Setup Template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,11 +40,19 @@ android {
 }
 
 dependencies {
+    // default implemenation
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // splash screen
+    implementation("androidx.core:core-splashscreen:1.0.0")
+
+    // test
     testImplementation("junit:junit:4.13.2")
+
+    // android test
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/dicoding/submissionbeginnerandroid/MainActivity.kt
+++ app/src/main/java/com/dicoding/submissionbeginnerandroid/MainActivity.kt
@@ -2,10 +2,17 @@ package com.dicoding.submissionbeginnerandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.dicoding.submissionbeginnerandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        // add splash screen
+        Thread.sleep(1000)
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
